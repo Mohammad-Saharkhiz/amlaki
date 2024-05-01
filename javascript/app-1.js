72f23cb938e21b37f6bd3e5a7287c21ce6569714
Mohammad Saharkhiz <you@example.com>
2024-05-01 17:50:45 +0330

    finally project

---
+++ javascript/app-1.js
@@ -0,0 +1,42 @@
+
+// const profile = document.querySelector(".user-profile")
+// const exit = document.querySelector(".profile-details")
+
+const navLinks = document.querySelector(".nav-links")
+const closeIcone = document.querySelector(".close-icone")
+const login = document.querySelector(".login")
+const cover = document.querySelector(".cover")
+
+navLinks.addEventListener('click', function () {
+    login.classList.add('login-show')
+    cover.classList.add('cover-show')
+})
+
+closeIcone.addEventListener('click', function () {
+    login.classList.remove('login-show')
+    cover.classList.remove('cover-show')
+})
+
+const menuItems = document.querySelectorAll(".menu--items")
+
+menuItems.forEach(item => {
+    item.addEventListener('click', function (e) {
+        e.preventDefault()
+        
+        let sectionClass = item.getAttribute("data-section")
+        let sectionOFFsetTop = document.querySelector(`.${sectionClass}`).offsetTop
+        window.scrollTo({
+            top: sectionOFFsetTop,
+            behavior: "smooth"
+        })
+    })
+})
+// profile.addEventListener('click', function () {
+//     if(exit.style.opacity == '1'){
+//         exit.style.opacity = '0'
+//         exit.style.visibility = 'hidden'
+//     }else{
+//         exit.style.opacity = '1'
+//         exit.style.visibility = 'visible'
+//     }
+// })
